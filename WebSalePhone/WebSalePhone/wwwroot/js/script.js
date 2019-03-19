161fb6692c2476b0b66b08e772b7afe5e6e477d3
Nguyễn Đức Thịnh <ndthinh2104@gmail.com>
2019-03-19 21:06:36 +0700

    add list phone view

--- WebSalePhone/WebSalePhone/wwwroot/js/script.js
+++ WebSalePhone/WebSalePhone/wwwroot/js/script.js
@@ -2,22 +2,4 @@
 	 
 })  
 
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    autoplay: true,
-    autoplayTimeout:3000,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-        },
-        600:{
-            items:1,
-        },
-        1000:{
-            items:1,
-            loop:true
-        }
-    }
-})
+
